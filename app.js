4fcfa3fb55c4b275f53324c2391dc56c85cee0d3
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2019-12-15 16:18:09 -0500

    Using different features of weather api.

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const request = require('request');
 
 const url = 'https://api.darksky.net/forecast/b1e9e52177c78752178bb4d00ee0c02b/37.8267,-122.4233';
 
-request({ url }, (error, response) => {
+request({ url, json: true }, (error, response) => {
   // console.log('error', error);
-  const data = JSON.parse(response.body);
-  console.log(data.currently);
+  const data = response.body.currently;
+  console.log(`${response.body.daily.data[0].summary} It is currently ${data.temperature} degrees out.  There is a ${data.precipProbability}% chance of rain.`)
 });
